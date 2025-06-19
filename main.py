14ef36c660f9a3a83adf74700bdbf6501d93f26a
Euan Jones <joneseuan28@gmail.com>
2025-06-19 03:01:24 +0100

    Added user driver selection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import fastf1
 import os
 
+# Handles cache directory to improve API performance
 try:
     os.mkdir('cache')
     print*("Cache directory successfully created.")
@@ -17,6 +18,7 @@ YEAR = 2024
 TRACK = 'Silverstone'
 TYPE = 'R'
 
+# Loads session data from FastF1 API call
 try:
     session = fastf1.get_session(YEAR, TRACK, TYPE)
     session.load()
@@ -26,14 +28,25 @@ except Exception as e:
 
 print("--------------------------------------------")
 
-print(f"{session.event['OfficialEventName']}")
-print(f"{session.event['EventDate']} \n")
-print("The competing drivers for this session are:")
+print(f"{session.event['OfficialEventName']} \n")
 
+print("The competing drivers for this session are:")
 available_drivers = session.drivers
 
-for driverNum in available_drivers:
-    current_driver = session.get_driver(driverNum)
-    print(f"{driverNum} - {current_driver['FullName']} ({current_driver['Abbreviation']})")
+# Loops through array of driver numbers and assigns them to driver object
+for driver_num in available_drivers:
+    current_driver = session.get_driver(driver_num)
+    print(f"{driver_num} - {current_driver['FullName']} ({current_driver['Abbreviation']})") # Uses driver object to pull data
 
 print("--------------------------------------------")
+
+selected_driver_id = input("Please select a driver using their number or abbreviated name: \n")
+
+# Creates driver object from user selection
+try:
+    selected_driver = session.get_driver(selected_driver_id)
+except Exception as e:
+    print(f"An error occured whilst searching for driver {selected_driver_id}: {e}")
+    exit(0)
+
+print(f"You have selected {selected_driver['BroadcastName']} ({selected_driver['CountryCode']}), part of the {selected_driver['TeamName']} F1 Team.")
