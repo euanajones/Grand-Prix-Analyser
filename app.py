2cd8f96d60b80bddb5728655ebb0fac02c86820f
Euan Jones <joneseuan28@gmail.com>
2025-09-03 14:06:45 +0100

    Added streamlit UI for ease of use

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from src import api_handler
+import fastf1
+import fastf1.plotting
 
 st.title("Formula One Grand Prix Analyser")
 
@@ -22,7 +24,7 @@ search = st.button('Search for Drivers', type="primary")
 
 if search:
     driver_data_status = st.text('Loading driver data...')
-    st.session_state.available_drivers = api_handler.getDrivers(season, track)
+    st.session_state.session, st.session_state.available_drivers = api_handler.getDrivers(season, track)
     driver_data_status.text('Driver data collected!')
 
 # Use session_state to populate the driver selectbox
@@ -36,5 +38,7 @@ select = st.button('Select Driver', type="primary")
 if select:
     driver_id = driver.split(" - ")[0]  # Extract driver ID from the selected option
     lap_data_status = st.text('Loading lap data...')
-    api_handler.getDriverLapData(season, track, driver_id)
-    # lap_data_status.text('Lap data collected!')
+    selected_driver, selected_driver_data = api_handler.getDriverLapData(st.session_state.session, driver_id)
+    lap_data_status.text('Lap data collected!')
+
+    api_handler.displayDriverData(st.session_state.session, selected_driver, selected_driver_data)
--- src/api_handler.py
+++ src/api_handler.py
@@ -1,6 +1,7 @@
 import fastf1
 from requests import session
 import streamlit as st
+from matplotlib import pyplot as plt
 
 def getDrivers(year, track):
     try:
@@ -20,12 +21,10 @@ def getDrivers(year, track):
         current_driver = session.get_driver(driver_num)
         session_drivers.append(f"{driver_num} - {current_driver['FullName']} ({current_driver['Abbreviation']})") # Uses driver object to pull data
 
-    return session_drivers
+    return session, session_drivers
 
-def getDriverLapData(year, track, driver_id):
+def getDriverLapData(session, driver_id):
     try:
-        session = fastf1.get_session(year, track, 'R')
-        session.load()
         selected_driver = session.get_driver(driver_id)
     except Exception as e:
         print(f"An error occured whilst searching for driver {driver_id}: {e}")
@@ -35,4 +34,68 @@ def getDriverLapData(year, track, driver_id):
 
     selected_driver_data = session.laps.pick_driver(driver_id)
 
-    return selected_driver_data
+    return selected_driver, selected_driver_data
+
+def displayDriverData(session, driver, driver_data):
+    # Converts pandas stint series into list
+    stint_data = driver_data['Stint'].to_list()
+
+    tyre_change = {}
+
+    for i in range(len(stint_data)-1):
+        current_stint = stint_data[i]
+
+        if stint_data[i+1] != current_stint:
+            current_stint = stint_data[i+1]
+
+            tyre_change[i+2] = 'UNKNOWN'
+
+    # Saves lap:compound data into a dictionary
+    for x in tyre_change:
+        compound = driver_data.pick_lap(x)['Compound']
+        tyre_change[x] = compound.to_string(index=False)
+
+    # Normalise data to remove In/Out Laps, SVC Laps, etc
+    laptime_seconds = driver_data['LapTime'].dt.total_seconds()
+    mean_laptime_seconds = laptime_seconds.mean()
+
+    # Normalised data stored as a dataframe, removes 20% of most extreme data (lower 10% and upper 10% extremes)
+    normal_driver_data = driver_data.loc[(laptime_seconds < mean_laptime_seconds * 1.1) & (laptime_seconds > mean_laptime_seconds * 0.9), :]
+
+    x = normal_driver_data['LapNumber']
+    y = normal_driver_data['LapTime'].dt.total_seconds()
+
+    # Plotting styles
+    compounds = ['SOFT', 'MEDIUM', 'HARD', 'INTERMEDIATE', 'WET', 'TEST_UNKNOWN', 'UNKNOWN']
+    compound_color_map = ['red', 'yellow', 'white', 'green', 'blue', 'gray', 'gray']
+
+    fastf1.plotting.setup_mpl(misc_mpl_mods=False, color_scheme='fastf1')
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label=driver['FullName'])
+
+    y_min, y_max = ax.get_ylim()
+
+    ax.axhspan(ymin=mean_laptime_seconds, ymax=y_max, color='red', alpha=0.3)
+    ax.axhspan(ymin=y_min, ymax=mean_laptime_seconds, color='green', alpha=0.3)
+
+
+    init_compound = driver_data.loc[driver_data['LapNumber'] == 1, 'Compound'].to_string(index=False)
+    init_comp_index = compounds.index(init_compound)
+    init_comp_color = compound_color_map[init_comp_index]
+
+    ax.axvline(x=1, color=init_comp_color, label=init_compound, ls=':')
+
+    for x in tyre_change.keys():
+        current_compound = tyre_change.get(x)
+        comp_index = compounds.index(current_compound)
+        comp_color = compound_color_map[comp_index]
+
+        ax.axvline(x=x, color=comp_color, label=current_compound, ls=':')
+
+    ax.set_xlabel('Lap Number')
+    ax.set_ylabel('Lap Time')
+    ax.set_title(f'Lap Time Per Lap at \n {session.event['EventName']}')
+    ax.legend()
+
+    st.pyplot(fig)
